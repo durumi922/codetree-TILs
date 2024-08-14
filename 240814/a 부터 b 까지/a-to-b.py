0f431b9b7a74922803d9f01f527430faad53942a
JiHye Ryu <155722710+durumi922@users.noreply.github.com>
2024-08-14 22:34:22 +0900

    Title: a 부터 b 까지, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/a 부터 b 까지/a-to-b.py
+++ 240814/a 부터 b 까지/a-to-b.py
@@ -4,7 +4,16 @@ print(a,end=" ")
 while a < b:
     if a%2!=0:
         a= a*2
+
+        if a>b:
+            break
+
         print(a,end=" ")
+
     elif a%2==0:
         a= a+3
+
+        if a>b:
+            break   
+
         print(a,end=" ")
