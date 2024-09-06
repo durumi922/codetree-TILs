15950b329a3cc97d8fb2313601440ccb445372d0
JiHye Ryu <155722710+durumi922@users.noreply.github.com>
2024-09-06 13:15:07 +0900

    Title: 윤년 횟수 구하기, Time: 127ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240906/윤년 횟수 구하기/number-of-leap-years.py
@@ -0,0 +1,17 @@
+n= int(input())
+yun =0 
+nomal=0
+
+for i in range(n+1):
+
+    if i%100==0 and i%400!=0:
+        nomal+=1
+
+    elif i % 4 ==0:
+        yun+=1    
+    
+    else:
+        nomal+=1
+    
+
+print(yun)
