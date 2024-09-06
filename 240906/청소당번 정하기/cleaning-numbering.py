c1e457515b7f96f4913ce4615dad652b77d29f57
JiHye Ryu <155722710+durumi922@users.noreply.github.com>
2024-09-06 11:35:24 +0900

    Title: 청소당번 정하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240906/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,20 @@
+cnt12=0
+cnt3=0
+cnt2=0
+n = int(input())
+
+
+for i in range(n):
+    if i==0:
+        continue
+    elif i%12==0:
+        cnt12 += 1
+    elif i%3==0:
+        cnt3 += 1
+    elif i%2==0:
+        cnt2 += 1
+    
+
+print(cnt2,cnt3,cnt12)
+
+# n이 아니라 i로 해야하네
